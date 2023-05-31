df17c695dc539eaa656d95b21d0ada7ee53c0fe1
encrypter-harsh <135134626+encrypter-harsh@users.noreply.github.com>
2023-05-31 19:18:34 +0530

    Add files via upload

---
+++ Day 2.py
@@ -0,0 +1,33 @@
+
+
+g=input("Enter F/f for female or M/m for male: \n")
+
+c=input("Enter S/s for Senior citizen or N/n for Non senior citizen: \n")
+
+a=int(input("Enter your principle amount \n"))
+
+t=int(input("Enter your time of Fixed Deposit"))
+
+if g=="f" or g=="F":
+	if c=="S"or c="s":
+		i= (a*t*8)/100
+	elif
+		c=="N"or c="n":
+		i=(a*t*6)/100
+	else:
+		print("Enter correct citizen choice")
+elif  g=="M" or g=="m":
+	if c=="S"or c="s":
+		i= (a*t*7)/100
+	elif:
+		c=="N"or c="n":
+		i=(a*t*5)/100
+	else:
+		print("Enter correct citizen choice")
+	
+else:
+	print("Enter correct gender choice")
+
+print("The interest will be",i)
+
+	
